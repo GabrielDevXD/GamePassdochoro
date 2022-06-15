f299a89bdce01e57d85f65d7afd5b0c64b006e07
GabrielDevXD1 <84517125+GabrielDevXD@users.noreply.github.com>
2022-06-14 21:32:12 -0300

    module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,19 @@ import { GenreModule } from './genre/genre.module';
 import { ProfileModule } from './profile/profile.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { HomepageModule } from './homepage/homepage.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [GamesModule, GenreModule, ProfileModule, UserModule, AuthModule],
+  imports: [
+    GamesModule,
+    GenreModule,
+    ProfileModule,
+    UserModule,
+    AuthModule,
+    HomepageModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
