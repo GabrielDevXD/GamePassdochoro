5078d2b750ca4a4f1df6b5050ab98abcfbe4ada5
GabrielDevXD1 <84517125+GabrielDevXD@users.noreply.github.com>
2022-06-14 22:10:53 -0300

    Fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'coe vini nunca te pedi nada, me da um 10 ai';
   }
 }
